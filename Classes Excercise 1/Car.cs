c805b61a7931bee82bdef73190dd0e4d95161f53
Zachariah Olenik <zjolenik139@gmail.com>
2022-08-19 11:48:45 -0400

    finished

---
+++ Classes Excercise 1/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Excercise_1
+{
+   public class Car
+    {
+
+        public Car()
+        {
+
+        }
+
+
+
+        public string Make { get; set; } 
+        public string Model { get; set; }   
+        public string Year { get; set; }
+
+
+
+    }
+    
+}
---
+++ Classes Excercise 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes_Excercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Focus";
+            myCar.Year = "2013";
+
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = "2016"
+            };
+
+            var chevy = new Car()
+            {
+                Make = "Chevy",
+                Model = "Impalla",
+                Year = "2001"
+            };
+            var carList = new List<Car>() { myCar, mazda, chevy };
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+
+
+
+
+        }
+    }
+}
